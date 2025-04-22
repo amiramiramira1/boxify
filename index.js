a5e5f1b480092203f095f26bf6160df102a048d2
amiramiramira1 <amiragohar11@gmail.com>
2025-04-22 19:56:07 +0200

    added express

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-
-
-
-
-
-
-
 app.listen(3000, () => {
-    console.log(`Server running on port 3000`);
-  });
+  console.log(`Server running on port 3000`);
+});
